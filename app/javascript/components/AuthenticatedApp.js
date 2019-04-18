cada1645ac35539213ad1bbf0f693af530ba5f91
a-visai <vce.visai@gmail.com>
2019-04-18 13:14:13 -0700

    Added Home Route

--- app/javascript/components/AuthenticatedApp.js
+++ app/javascript/components/AuthenticatedApp.js
@@ -3,14 +3,18 @@ import PropTypes from "prop-types"
 import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom'
 import Confessions from './pages/Confessions'
 import Confess from './pages/Confess'
+import Home from './pages/Home'
 
 class AuthenticatedApp extends React.Component {
   render () {
     return (
       <Router>
         <React.Fragment>
+        <Link to='/home'>Home</Link>
+        <Link to='/confessions'>Confessions</Link>
         <Link to='/confess'>Confess</Link>
           <Switch>
+            <Route path='/home' component={Home}/>
             <Route path='/confess' component={Confess}/>
             <Route path='/confessions' component={Confessions}/>
           </Switch>
