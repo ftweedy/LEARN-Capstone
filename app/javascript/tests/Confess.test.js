58c4a1ceca0fd6557160cdf977844882ed7ca2fd
Francis <tweedyfrancis@gmail.com>
2019-04-29 10:26:48 -0700

    last of render tests for react

--- app/javascript/tests/Confess.test.js
+++ app/javascript/tests/Confess.test.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Confessions from '../components/pages/Confessions';
+import Confess from '../components/pages/Confess';
 
-it('Confessions renders', () => {
+it('Confess renders', () => {
   const div = document.createElement('div');
   fetch.mockResponse(JSON.stringify([{name: 'test'}, {name: 'test2'}]))
 
-  ReactDOM.render(<Confessions current_user={{id: 1}}/>, div);
+  ReactDOM.render(<Confess/>, div);
 });
